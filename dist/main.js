a33794e2242f19659d242a261cc07a21600ab84c
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro.local>
2020-11-11 15:00:47 +0530

    project setup

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log("Hello"),console.log("How are you?"),console.log("How are you")})();
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import app from './app';
 console.log(app.greet);
 console.log(app.greet1);
 
-// const x = 'hello';
+const x = 'hello';
+console.log('x', x);
 
 console.log('How are you');
 
