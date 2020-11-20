8577fd63d7a693e208722a992f65c9e92b65b7f1
raj-patel-sys <patelrajj1904@gmail.com>
2020-11-20 23:08:34 +0530

    state done

--- src/classPrac.js
+++ src/classPrac.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable react/static-property-placement */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
@@ -13,10 +11,34 @@ export default class ClassApp extends Component {
     message: 'i am good',
   };
 
+  state = {
+    string: 'String one',
+    string1: 'String Two',
+    textvalue: '',
+  };
+
+  onPress = () => {
+    // this.setState({
+    //   string: 'S0 changed',
+    //   string1: 'S1 changed',
+    // });
+    alert(this.state.textvalue);
+  };
+
+  onChange = event => {
+    this.setState({
+      textvalue: event.target.value,
+    });
+  };
+
   render() {
+    console.log('Anything');
     const { greet, message } = this.props;
+    const { string, string1, textvalue } = this.state;
     return (
       <div>
+        <h1>{string}</h1>
+        <h1>{string1}</h1>
         <h1
           style={{
             color: 'red',
@@ -31,10 +53,16 @@ export default class ClassApp extends Component {
           }}>
           {message}
         </h1>
-        Login here <input type="text" />
+        Login here
+        <input type="text" value={textvalue} onChange={this.onChange} />
+        <button type="button" onClick={this.onPress}>
+          Click me
+        </button>
         <br />
         <br />
-        Password <input type="Password" />
+        Password
+        <input type="Password" />
+        <button type="button"> Don't Click me </button>
       </div>
     );
   }
