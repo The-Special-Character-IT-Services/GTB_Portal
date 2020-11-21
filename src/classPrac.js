656f273d7adfa3f3902520374972d57ffa67eb80
raj-patel-sys <patelrajj1904@gmail.com>
2020-11-21 18:56:36 +0530

    lifecycle Prac

--- src/classPrac.js
+++ src/classPrac.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+// This is of mounting lifecycle
+// constructor
+// getDerivedStateFromProps
+// render
+// componentDidMount
+
+// this is of updating lifecycle
+// getDerivedStateFromProps
+// shouldComponentUpdate
+// render
+// componentDidUpdate
+
 export default class ClassApp extends Component {
   static propTypes = {
     greet: PropTypes.string.isRequired,
@@ -11,35 +23,66 @@ export default class ClassApp extends Component {
     message: 'i am good',
   };
 
-  state = {
-    string: 'String one',
-    string1: 'String Two',
-    textvalue: '',
-  };
+  // state = {};
+
+  constructor(props) {
+    // call only once
+    super(props);
+    this.state = {
+      string: props.greet,
+      string1: props.message,
+      textvalue: '',
+    };
+    console.log('in Constructor', props);
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('getDerivedStateFromProps', props, state);
+    return {
+      ...state,
+      myname: props.greet ? 'Raj' : '',
+    };
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    console.log(document.getElementById('btn'));
+    document.addEventListener('copy', data => {
+      console.log(data);
+    });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.textvalue !== '') {
+      return true;
+    }
+    return false;
+  }
 
   onPress = () => {
-    // this.setState({
-    //   string: 'S0 changed',
-    //   string1: 'S1 changed',
-    // });
-    alert(this.state.textvalue);
+    this.setState({
+      string: 'S0 changed',
+      string1: 'S1 changed',
+    });
+    // alert(this.state.textvalue);
   };
 
-  onChange = event => {
+  onChangeString = event => {
     this.setState({
       textvalue: event.target.value,
     });
   };
 
   render() {
-    console.log('Anything');
+    console.log('In Render');
     const { greet, message } = this.props;
-    const { string, string1, textvalue } = this.state;
+    const { string, string1, textvalue, myname } = this.state;
     return (
       <div>
         <h1>{string}</h1>
         <h1>{string1}</h1>
-        <h1
+        <h1>{myname}</h1>
+        {/* <h1
           style={{
             color: 'red',
             backgroundColor: 'blue',
@@ -52,10 +95,10 @@ export default class ClassApp extends Component {
             textAlign: 'center',
           }}>
           {message}
-        </h1>
+        </h1> */}
         Login here
-        <input type="text" value={textvalue} onChange={this.onChange} />
-        <button type="button" onClick={this.onPress}>
+        <input type="text" value={textvalue} onChange={this.onChangeString} />
+        <button id="btn" type="button" onClick={this.onPress}>
           Click me
         </button>
         <br />
