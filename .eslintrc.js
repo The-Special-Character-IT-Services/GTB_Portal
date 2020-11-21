b9192ed7e54c96baf026ef1577bfdc9c107a9462
unknown <patelrajj1904@gmail.com>
2020-11-21 12:06:57 +0530

    commited

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'arrow-parens': [1, 'as-needed'],
+    'react/state-in-constructor': [true, 'never'],
   },
 };
--- src/ClassApp.js
+++ src/ClassApp.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-export default class ClassApp extends Component {
-  static propTypes = {
-    greet: PropTypes.string.isRequired,
-    message: PropTypes.string,
-  };
 
+export default class ClassApp extends Component {
   static defaultProps = {
     ans: 'fine',
   };
-
+  state = {
+    text: 'hello',
+    text1: 'text1',
+    text2: '',
+  };
+  onClick = () => {
+    alert(this.state.text2);
+    // this.setState({
+    //   // text: 'text changed',
+    // });
+  };
+  onChange = event => {
+    this.setState({ text2: event.target.value });
+  };
   render() {
+    console.log('render method');
     const { ask, ans } = this.props;
+    const { text, text1, text2 } = this.props;
+
     return (
       <div>
+        <h1>{text}</h1>
         <h1>{ask}</h1>
         <h1>{ans}</h1>
-        <input type='password' />
+        <h1>{text1}</h1>
+        <button type='button' onClick={this.onClick}>
+          click me
+        </button>
+        <input type='text' value={text} onChange={this.onChange} />
       </div>
     );
   }
