90eb0d0bb5969971897352cf95180a42c4b2a6ff
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro.local>
2020-11-23 13:52:20 +0530

    commit code

--- src/ClassApp.js
+++ src/ClassApp.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Child1 from './Child1';
+import Child2 from './Child2';
 
 // props value change
 // stae valuee change
@@ -38,16 +40,19 @@ export default class ClassApp extends Component {
   constructor(props) {
     // call only 1nce
     super(props);
-    console.log(props);
+    // console.log(props);
     this.state = {
       text: props.greet,
       text1: props.message,
       textValue: '',
+      user: {
+        name: 'Yagensh',
+      },
     };
   }
 
   static getDerivedStateFromProps(props, state) {
-    console.log('getDerivedStateFromProps', props, state);
+    // console.log('getDerivedStateFromProps', props, state);
     return {
       ...state,
       name: props.greet ? 'Yagnesh' : '',
@@ -59,24 +64,25 @@ export default class ClassApp extends Component {
     //   return true;
     // }
     // return false;
+    return true;
   }
 
   componentDidMount() {
     // call only 1nce
     // throw ne Error('err');
-    console.log('componentDidMount');
-    console.log(document.getElementById('btn'));
+    // console.log('componentDidMount');
+    // console.log(document.getElementById('btn'));
     // document.addEventListener('mousemove', data => {
     //   console.log(data);
     // });
     this.timer = setTimeout(() => {
-      console.log('hello');
+      // console.log('hello');
     }, 10000);
 
     // api call
   }
 
-  static getSnapshotBeforeUpdate() {
+  getSnapshotBeforeUpdate() {
     return 10;
   }
 
@@ -97,9 +103,13 @@ export default class ClassApp extends Component {
   }
 
   onClick = () => {
+    this.state.user.name = 'rohit';
     this.setState({
-      text: 'Text Changed',
+      user: this.state.user,
     });
+    // this.setState({
+    //   text: 'Text Changed',
+    // });
     // alert(this.state.textValue);
   };
 
@@ -110,14 +120,16 @@ export default class ClassApp extends Component {
   };
 
   render() {
-    console.log('render method');
+    console.log('Class App');
     // const { greet, message } = this.props;
-    const { text, text1, textValue, name } = this.state;
+    const { text, text1, textValue, name, user } = this.state;
     return (
       <div>
-        <h1>{text}</h1>
-        <h1>{text1}</h1>
-        <h1>{name}</h1>
+        {/* <h1>{text}</h1> */}
+        {/* <h1>{text1}</h1> */}
+        <h1>{user.name}</h1>
+        <Child1 data={user} />
+        <Child2 />
         {/* <h1>{greet}</h1> */}
         {/* <h1>{message}</h1> */}
         <input type="text" value={textValue} onChange={this.onChangeText} />
