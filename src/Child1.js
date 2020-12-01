2afff3b040ab2cda3640adb0283e7a4eec648231
shuklakhushi <180770116110@socet.edu.in>
2020-12-01 12:50:03 +0530

    done

--- src/Child1.js
+++
@@ -1,8 +0,0 @@
-import React, { memo } from 'react';
-
-const Child1 = ({ data }) => {
-  console.log('Child1');
-  return <div>Child1</div>;
-};
-
-export default memo(Child1);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Todo from './Todo';
-import './index.css';
-import './tailwind.css';
 
 ReactDOM.render(<Todo />, document.getElementById('root'));
