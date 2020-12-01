0eda26fb7492e518b539e09aeff9b5ab1df8af68
unknown <patelrajj1904@gmail.com>
2020-12-01 13:47:29 +0530

    commit

--- src/Todo/index.js
+++ src/Todo/index.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
 // import PropTypes from 'prop-types';
-
 export default class index extends PureComponent {
   state = {
     textTodo: '',
@@ -23,8 +22,28 @@ export default class index extends PureComponent {
     });
   };
 
+  onCompleteTodo = id => {
+    const { todoList } = this.state;
+    const i = todoList.findIndex(x => x.id === id);
+    this.setState({
+      todoList: [
+        ...todoList.slice(0, i),
+        { ...todoList[i], isDone: !todoList[i].isDone },
+        ...todoList.slice(i + 1),
+      ],
+    });
+  };
+
+  deleteTodo = id => {
+    const { todoList } = this.state;
+    const i = todoList.findIndex(x => x.id === id);
+    this.setState({
+      todoList: [...todoList.slice(0, i), ...todoList.slice(i + 1)],
+    });
+  };
+
   render() {
-    const { textTodo } = this.state;
+    const { textTodo, todoList } = this.state;
     return (
       <div>
         <h1>To-Do List</h1>
@@ -32,6 +51,17 @@ export default class index extends PureComponent {
           <input type="text" value={textTodo} onChange={this.onChangeText} />
           <button type="submit">SUBMIT</button>
         </form>
+        <div>
+          {todoList.map(item => (
+            <div key={item.id}>
+              <span>{item.textTodo}</span>
+              <button onClick={() => this.onCompleteTodo(item.id)}>
+                {item.isDone ? 'Not done' : 'Done'}
+              </button>
+              <button onClick={() => this.deleteTodo(item.id)}>Remove</button>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
